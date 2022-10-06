0b5a5e80e2d477fd2f675f95fedde338e2a5feb8
DinoJShen <shenps9@gmail.com>
2022-10-06 13:50:20 +0800

    s

--- app/Admin/Controllers/AgentStockRecordController.php
+++ app/Admin/Controllers/AgentStockRecordController.php
@@ -113,7 +113,9 @@ class AgentStockRecordController extends AdminController
                     // })
                     ->when(request('agentstock') != null && Admin::user()->inRoles(['administrator', 'company']), function ($q) {
                         return $q->when(request('agentstock')['agent_id'] != null, function ($q) {
-                            return $q->where('agentstock.agent_id', request('agentstock')['agent_id']);
+                            return $q->with(["agentstock" => function ($q) {
+                                $q->where('agentstock.agent_id', '=', request('agentstock')['agent_id']);
+                            }]);
                         });
                     })->sum('quantity');
                 $htmltext .= "<div class='badge bg-yellow' style='padding: 10px;margin-right:10px;'>" . $data->name . ": " . $quantity . "</div>";
